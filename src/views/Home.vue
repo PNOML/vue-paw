8d5aff6543093efd74b45d6becc27c47d6cff494
Vlad Timofeev <vladtimspnoml@gmail.com>
2020-05-27 11:33:12 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,104 +1,6 @@
 <template>
   <div class="main-wrapper main-body">
-    <header>
-      <div class="main-wrapper">
-        <div>
-          <router-link to="/"><img
-              src="images/logo.png"
-              class="logo"
-            >
-          </router-link>
-          <div class="top-menu">
-            <div>
-              <a
-                class="drop-menu"
-                href=""
-              >
-                <fa-icon
-                  class="my-icons"
-                  :icon="['fas', 'bars']"
-                />
-              </a>
-            </div>
-            <div class="top-menu-center">
-              <a
-                href=""
-                class="top-menu_links"
-              >
-                <fa-icon
-                  :icon="['fas', 'gift']"
-                  size="2x"
-                />
-                <div class="ml-10">Бонусы и акции</div>
-              </a>
-              <a
-                href=""
-                class="top-menu_links"
-              >
-                <fa-icon
-                  :icon="['fas', 'phone-volume']"
-                  size="2x"
-                />
-                <div class="ml-10">
-                  <div>8 903 111 09 23</div>
-                  <div class="top-menu_links_small">Прием звонков c 10:00-21:00</div>
-                </div>
-              </a>
-              <a
-                href=""
-                class="top-menu_links"
-              >
-                <fa-icon
-                  :icon="['fas', 'truck']"
-                  size="2x"
-                />
-                <div class="ml-10">
-                  <div>Бесплатная доставка</div>
-                  <div class="top-menu_links_small">От 3333 руб</div>
-                </div>
-              </a>
-            </div>
-            <div class="search">
-              <input
-                class="search-txt"
-                type="text"
-                name=""
-                placeholder="Начните искать товар..."
-              >
-              <a
-                href="#"
-                class="search-btn"
-              >
-                <fa-icon
-                  class="white"
-                  :icon="['fas', 'search']"
-                />
-              </a>
-            </div>
-            <div class="user-block">
-              <a
-                class="user-block_links"
-                href=""
-              >
-                <fa-icon
-                  :icon="['fas', 'shopping-cart']"
-                  size="2x"
-                />
-              </a>
-              <a
-                class="user-block_links"
-                href=""
-              >
-                <fa-icon
-                  :icon="['fas', 'user']"
-                  size="2x"
-                />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
+    <Header />
     <main>
       <section class="nav-animal-group">
         <div class="main-wrapper">
@@ -135,118 +37,20 @@
         </div>
       </section>
     </main>
-    <footer>
-      <div class="main-wrapper">
-        <div>
-          <div>
-            <router-link to="/">
-              <img
-                src="images/footer-logo (1).png"
-                class="footer-logo"
-              >
-            </router-link>
-          </div>
-          <div class="footer-menu">
-            <h3>Меню</h3>
-            <ul>
-              <li><a
-                  class="footer-links"
-                  href=""
-                >Главная</a></li>
-              <li><a
-                  class="footer-links"
-                  href=""
-                >Товары</a></li>
-              <li><a
-                  class="footer-links"
-                  href=""
-                >Доставк</a></li>
-              <li><a
-                  class="footer-links"
-                  href=""
-                >Оплата</a></li>
-              <li><a
-                  class="footer-links"
-                  href=""
-                >Контакты</a></li>
-            </ul>
-          </div>
-          <div class="animals">
-            <h3>Животные</h3>
-            <ul>
-              <li><a
-                  class="footer-links"
-                  href=""
-                >Котейки</a></li>
-              <li><a
-                  class="footer-links"
-                  href=""
-                >Собакены</a></li>
-              <li><a
-                  class="footer-links"
-                  href=""
-                >Амфибии</a></li>
-              <li><a
-                  class="footer-links"
-                  href=""
-                >Парящие</a></li>
-              <li><a
-                  class="footer-links"
-                  href=""
-                >Другие</a></li>
-            </ul>
-          </div>
-          <div class="other-info">
-            <h3>Дополнительно</h3>
-            <ul>
-              <li><a
-                  class="footer-links"
-                  href=""
-                >Дополнительно</a></li>
-            </ul>
-          </div>
-          <div class="follow-up">
-            <h3>Следуй за нами</h3>
-            <ul>
-              <li class="footer-social-media">
-                <a href="">
-                  <fa-icon :icon="['fab', 'instagram']" />
-                </a>
-              </li>
-              <li class="footer-social-media">
-                <a href="">
-                  <fa-icon :icon="['fab', 'telegram-plane']" />
-                </a>
-              </li>
-              <li class="footer-social-media">
-                <a href="">
-                  <fa-icon :icon="['fab', 'vk']" />
-                </a>
-              </li>
-              <li class="footer-social-media">
-                <a href="">
-                  <fa-icon :icon="['fab', 'whatsapp']" />
-                </a>
-              </li>
-              <li class="footer-social-media">
-                <a href="">
-                  <fa-icon :icon="['fab', 'viber']" />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from "@/components/app/Header";
+import Footer from "@/components/app/Footer";
 import Navbar from "@/components/app/Navbar";
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Header,
+    Footer
   }
 };
 </script>
