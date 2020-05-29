9e971c380c17ab5f10cb780436b1568c9110fa2d
Vlad Timofeev <vladtimspnoml@gmail.com>
2020-05-29 19:49:02 +0300

    Update SidebarLC.vue

---
+++ src/components/app/LC/SidebarLC.vue
@@ -0,0 +1,39 @@
+<template>
+  <aside class="sidebar">
+    <ul>
+
+      <router-link
+        v-for="cat in categories"
+        :key="cat.title"
+        tag="li"
+        active-class="active"
+        to="/goods"
+        exact
+      >
+        <a
+          class="sidebar-links"
+          href="#"
+        >{{cat.title}}
+        </a>
+      </router-link>
+
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    categories: [
+      { title: "Корма" },
+      { title: "Консервы" },
+      { title: "Наполнители" },
+      { title: "Лакомства" },
+      { title: "Аксессуары" },
+      { title: "Игрушки" },
+      { title: "Уход" },
+      { title: "Прочее" }
+    ]
+  })
+};
+</script>
