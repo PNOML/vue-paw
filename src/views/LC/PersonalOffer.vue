df36a075356ac9318b9717e933ac37e9d8bcd72b
Vlad Timofeev <vladtimspnoml@gmail.com>
2020-05-29 19:58:16 +0300

    Create PersonalOffer.vue

---
+++ src/views/LC/PersonalOffer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-wrapper main-body">
+    <Header />
+    <main class="purchase-container">
+      <div class="main-wrapper ">
+        <SidebarLC />
+        <div class="sale-block">
+          <Navbar />
+          <section class="profile">
+            <h1>Уникальное предложение для Вас</h1>
+          </section>
+        </div>
+      </div>
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/app/Header";
+import Footer from "@/components/app/Footer";
+import Navbar from "@/components/app/Navbar";
+import SidebarLC from "@/components/app/LC/SidebarLC";
+
+export default {
+  name: "goods",
+  components: {
+    Header,
+    Footer,
+    Navbar,
+    SidebarLC
+  }
+};
+</script>
