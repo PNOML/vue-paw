6f2577c1f9769947d5bfe45096105d403ec16f3f
Vlad Timofeev <vladtimspnoml@gmail.com>
2020-05-29 19:40:17 +0300

    Create BonusPage.vue

---
+++ src/views/BonusPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main-wrapper main-body">
+    <Header />
+    <main>
+      <section class="nav-animal-group">
+        <div class="main-wrapper">
+          <Navbar />
+        </div>
+      </section>
+      <div class="main-wrapper">
+        <section class="contacts-info">
+          <div class="main-wrapper">
+            <h1>Бонусы</h1>
+          </div>
+        </section>
+      </div>
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/app/Header";
+import Footer from "@/components/app/Footer";
+import Navbar from "@/components/app/Navbar";
+
+export default {
+  name: "goods",
+  components: {
+    Header,
+    Footer,
+    Navbar
+  }
+};
+</script>
