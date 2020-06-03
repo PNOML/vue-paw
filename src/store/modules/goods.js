00e60d12a0cfbc038d228cb61818c1003dc4bf1e
Vlad Timofeev <vladtimspnoml@gmail.com>
2020-06-03 10:54:23 +0300

    Update goods.js

--- src/store/modules/goods.js
+++ src/store/modules/goods.js
@@ -1,6 +1,23 @@
 export default {
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {}
+  state: {
+    goods: []
+  },
+  getters: {
+    allGoods(state) {
+      return state.goods
+    }
+  },
+  mutations: {
+    updateGoods(state, goods) {
+      state.goods = goods;
+    }
+  },
+  actions: {
+    async fetchGoods(ctx) {
+      const res = await fetch("http://localhost:3000/goods");
+      const goods = await res.json();
+
+      ctx.commit('updateGoods', goods)
+    }
+  }
 }
