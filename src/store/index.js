57751c657a8f624f5cdbdc08dec90cce303c29e8
Vlad Timofeev <vladtimspnoml@gmail.com>
2020-06-03 11:31:51 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -6,37 +6,6 @@ import goods from './modules/goods'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  // state: {
-  //   goods: []
-  // },
-  // getters: {
-  //   GOODS(state) {
-  //     console.log(state.goods)
-  //     return state.goods;
-  //   }
-  // },
-  // mutations: {
-  //   SET_GOODS_TO_STATE: (state, goods) => {
-  //     state.goods = goods;
-  //   }
-  // },
-  // actions: {
-  //   GET_GOODS_FROM_API({ commit }) {
-  //     return axios('http://localhost:3000/goods', {
-  //       method: "GET"
-  //     })
-  //       .then(goods => {
-  //         console.log(goods)
-  //         commit('SET_GOODS_TO_STATE', goods.data);
-  //         return goods;
-  //       })
-  //       .catch(e => {
-  //         console.log(e);
-  //         return e;
-  //       })
-  //   }
-  // },
-
   modules: {
     goods
   }
