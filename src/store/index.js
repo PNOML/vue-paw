926b299e68f0e8a824d6e1178fd67c4930634f37
Vlad Timofeev <vladtimspnoml@gmail.com>
2020-06-03 10:38:35 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,36 +5,36 @@ import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    goods: []
-  },
-  getters: {
-    GOODS(state) {
-      console.log(state.goods)
-      return state.goods;
-    }
-  },
-  actions: {
-    GET_GOODS_FROM_API({ commit }) {
-      return axios('http://localhost:3000/goods', {
-        method: "GET"
-      })
-        .then(goods => {
-          console.log(goods)
-          commit('SET_GOODS_TO_STATE', goods.data);
-          return goods;
-        })
-        .catch(e => {
-          console.log(e);
-          return e;
-        })
-    }
-  },
-  mutations: {
-    SET_GOODS_TO_STATE: (state, goods) => {
-      state.goods = goods;
-    }
-  },
+  // state: {
+  //   goods: []
+  // },
+  // getters: {
+  //   GOODS(state) {
+  //     console.log(state.goods)
+  //     return state.goods;
+  //   }
+  // },
+  // mutations: {
+  //   SET_GOODS_TO_STATE: (state, goods) => {
+  //     state.goods = goods;
+  //   }
+  // },
+  // actions: {
+  //   GET_GOODS_FROM_API({ commit }) {
+  //     return axios('http://localhost:3000/goods', {
+  //       method: "GET"
+  //     })
+  //       .then(goods => {
+  //         console.log(goods)
+  //         commit('SET_GOODS_TO_STATE', goods.data);
+  //         return goods;
+  //       })
+  //       .catch(e => {
+  //         console.log(e);
+  //         return e;
+  //       })
+  //   }
+  // },
 
   modules: {
   }
